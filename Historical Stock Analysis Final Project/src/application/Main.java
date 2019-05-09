4128e002a1140d8c36d11da78bca79a83e90cec2
chavez102 <bryanchavez102@gmail.com>
2019-05-09 10:39:26 -0400

    Setting Page and Controller for Menu Page

--- Historical Stock Analysis Final Project/src/application/Main.java
+++ Historical Stock Analysis Final Project/src/application/Main.java
@@ -1,19 +1,30 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public static String Stock_name="name of stock";
+	
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+			
+			
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main_Page.fxml"));
 			Scene scene = new Scene(root,841,698);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
 			primaryStage.setScene(scene);
 			
 			primaryStage.show();
@@ -25,4 +36,35 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	
+		
+		
+	public static void SwitchPageto(ActionEvent event,String fxmldocumentstr) {
+			try {
+			Parent root = FXMLLoader.load(application.Main.class.getResource(fxmldocumentstr)); 
+			Scene scene = new Scene(root,841,698);
+			Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+			window.setScene(scene);
+			window.show();
+			
+			
+			
+			}catch(Exception e) { e.printStackTrace();}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+
 }
